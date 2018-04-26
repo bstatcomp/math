8d886c1e5bb3e063752762c223557f4022e175cd
Rok Češnovar <rok.cesnova@fri.uni-lj.si>
2018-04-26 15:47:09 +0200

    added add and subtract tests

--- test/unit/math/gpu/copy_gpu_test.cpp
+++ test/unit/math/gpu/copy_gpu_test.cpp
@@ -73,3 +73,18 @@ TEST(MathMatrixGPU, copy_triangular_transposed_m_pass) {
   EXPECT_EQ(2, m0_dst(1,0));
   EXPECT_EQ(4, m0_dst(1,1));  
 }
+
+TEST(MathMatrixGPU, copy_submatrix_exception) {
+  stan::math::matrix_d d1;
+
+  d1.resize(3, 3);
+  stan::math::matrix_gpu d11(d1);
+  stan::math::matrix_gpu d22(2, 2);
+  EXPECT_NO_THROW(stan::math::copy_submatrix(d11, d22, 1, 1, 0, 0, 2, 2));
+  
+  
+  EXPECT_THROW(stan::math::copy_submatrix(d11, d22, 1, 1, 0, 0, 4, 4), std::domain_error);
+  EXPECT_THROW(stan::math::copy_submatrix(d11, d22, 4, 4, 0, 0, 2, 2), std::domain_error);
+  EXPECT_THROW(stan::math::copy_submatrix(d11, d22, 1, 1, 3, 3, 4, 4), std::domain_error);
+  EXPECT_THROW(stan::math::copy_submatrix(d11, d22, 1, 1, 3, 3, 3, 3), std::domain_error);
+}

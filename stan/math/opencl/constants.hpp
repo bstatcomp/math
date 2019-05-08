3bbb2e3b523192e428088c8ab8ef95571b3b8d9d
tadej <tadej.c@gmail.com>
2019-05-08 12:08:11 +0200

    adeded macro for common c++/opencl code

--- stan/math/opencl/constants.hpp
+++ stan/math/opencl/constants.hpp
@@ -1,10 +1,19 @@
 #ifndef STAN_MATH_OPENCL_CONSTANTS_HPP
 #define STAN_MATH_OPENCL_CONSTANTS_HPP
 #ifdef STAN_OPENCL
+#include<stan/math/opencl/kernel_code_macros.hpp>
+
 namespace stan {
 namespace math {
-enum class TriangularViewCL { Lower = 0, Upper = 1, Entire = 2 };
-enum class TriangularMapCL { UpperToLower = 0, LowerToUpper = 1 };
+// \cond
+static const char* enums_kernel_code = COMMON_CODE(
+        // \endcond
+        enum class TriangularViewCL { Lower = 0, Upper = 1, Entire = 2 };
+        enum class TriangularMapCL { UpperToLower = 0, LowerToUpper = 1 };
+        // \cond
+        );
+// \endcond
+
 }  // namespace math
 }  // namespace stan
 #endif
---
+++ stan/math/opencl/kernel_code_macros.hpp
@@ -0,0 +1,21 @@
+#ifndef STAN_MATH_OPENCL_KERNEL_CODE_MACROS_HPP
+#define STAN_MATH_OPENCL_KERNEL_CODE_MACROS_HPP
+#ifdef STAN_OPENCL
+#include <boost/algorithm/string/replace.hpp>
+#include <string>
+
+// Used for importing the OpenCL kernels at compile time.
+// There has been much discussion about the best ways to do this:
+// https://github.com/bstatcomp/math/pull/7
+// and https://github.com/stan-dev/math/pull/966
+#ifndef STRINGIFY
+#define STRINGIFY(...) #__VA_ARGS__
+#endif
+
+#ifndef COMMON_CODE
+#define COMMON_CODE(...) boost::replace_all_copy(std::string(STRINGIFY(__VA_ARGS__)),"enum class", "enum").c_str(); __VA_ARGS__
+#endif
+
+
+#endif
+#endif

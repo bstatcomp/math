4a566b98113dc8ee73edb85bcf4e554b3e56d1e4
tadej <tadej.c@gmail.com>
2019-08-26 19:22:23 +0200

    add transposition

---
+++ stan/math/opencl/kernel_generator/transpose.hpp
@@ -0,0 +1,70 @@
+#ifndef STAN_MATH_OPENCL_KERNEL_GENERATOR_TRANSPOSE_HPP
+#define STAN_MATH_OPENCL_KERNEL_GENERATOR_TRANSPOSE_HPP
+#ifdef  STAN_OPENCL
+
+#include <stan/math/opencl/matrix_cl_view.hpp>
+#include <stan/math/prim/meta.hpp>
+#include <stan/math/opencl/kernel_generator/type_str.hpp>
+#include <stan/math/opencl/kernel_generator/name_generator.hpp>
+#include <stan/math/opencl/kernel_generator/operation.hpp>
+#include <stan/math/opencl/kernel_generator/as_operation.hpp>
+#include <stan/math/opencl/kernel_generator/is_usable_as_operation.hpp>
+#include <string>
+#include <type_traits>
+#include <set>
+#include <utility>
+
+namespace stan{
+namespace math{
+
+template<typename Derived, typename T>
+class transpose__ : public operation<Derived, typename std::remove_reference_t<T>::ReturnScalar> {
+public:
+    using ReturnScalar = typename std::remove_reference_t<T>::ReturnScalar;
+    using base = operation<Derived, ReturnScalar>;
+    using base::var_name;
+    using base::instance;
+
+    transpose__(T&& a) : a_(std::forward<T>(a)) {}
+
+    kernel_parts generate(name_generator& ng, std::set<int>& generated, const std::string& i, const std::string& j) const{
+      return a_.generate(ng, generated, j, i);
+    }
+
+    void set_args(std::set<int>& generated, cl::Kernel& kernel, int& arg_num) const{
+      if(generated.count(instance)==0) {
+        generated.insert(instance);
+        a_.set_args(generated, kernel, arg_num);
+      }
+    }
+
+    void add_event(cl::Event& e) const {
+      a_.add_event(e);
+    }
+
+    matrix_cl_view view() const {
+      return transpose(a_.view());
+    }
+
+    int rows() const {
+      return a_.cols();
+    }
+
+    int cols() const {
+      return a_.rows();
+    }
+
+protected:
+    const T a_;
+};
+
+template<typename T, typename = enable_if_none_arithmetic_all_usable_as_operation <T>>
+auto transpose(T&& a) -> const transpose__<decltype(as_operation(std::forward<T>(a)))> {
+  return transpose__<decltype(as_operation(std::forward<T>(a)))>(as_operation(std::forward<T>(a)));
+}
+
+}
+}
+
+#endif
+#endif
---
+++ test/unit/math/opencl/kernel_generator/transpose_test.cpp
@@ -0,0 +1,59 @@
+#include <stan/math/prim/mat/fun/Eigen.hpp>
+#include <stan/math/opencl/kernel_generator/unary_function.hpp>
+#include <stan/math/opencl/matrix_cl.hpp>
+#include <stan/math/opencl/copy.hpp>
+#include <stan/math.hpp>
+#include <gtest/gtest.h>
+
+using Eigen::MatrixXd;
+using stan::math::matrix_cl;
+
+#define EXPECT_MATRIX_NEAR(A, B, DELTA) \
+  for (int i = 0; i < A.size(); i++)    \
+    EXPECT_NEAR(A(i), B(i), DELTA);
+
+TEST(MathMatrixCL, transpose_test){
+  MatrixXd m(3, 2);
+  m << 1.1, 1.2, 1.3, 1.4, 1.5, 1.6;
+
+  matrix_cl<double> m_cl(m);
+  auto tmp = stan::math::transpose(m_cl);
+  matrix_cl<double> res_cl = tmp;
+
+  MatrixXd res = stan::math::from_matrix_cl(res_cl);
+  MatrixXd correct = stan::math::transpose(m);
+  EXPECT_EQ(correct.rows(), res.rows());
+  EXPECT_EQ(correct.cols(), res.cols());
+  EXPECT_MATRIX_NEAR(correct,res,1e-9);
+}
+
+TEST(MathMatrixCL, double_transpose_test){
+  MatrixXd m(3, 2);
+  m << 1.1, 1.2, 1.3, 1.4, 1.5, 1.6;
+
+  matrix_cl<double> m_cl(m);
+  auto tmp = stan::math::transpose(stan::math::transpose(m_cl));
+  matrix_cl<double> res_cl = tmp;
+
+  MatrixXd res = stan::math::from_matrix_cl(res_cl);
+  MatrixXd correct = m;
+  EXPECT_EQ(correct.rows(), res.rows());
+  EXPECT_EQ(correct.cols(), res.cols());
+  EXPECT_MATRIX_NEAR(correct,res,1e-9);
+}
+
+TEST(MathMatrixCL, double_transpose_accepts_lvalue_test){
+  MatrixXd m(3, 2);
+  m << 1.1, 1.2, 1.3, 1.4, 1.5, 1.6;
+
+  matrix_cl<double> m_cl(m);
+  auto tmp2 = stan::math::transpose(m_cl);
+  auto tmp = stan::math::transpose(tmp2);
+  matrix_cl<double> res_cl = tmp;
+
+  MatrixXd res = stan::math::from_matrix_cl(res_cl);
+  MatrixXd correct = m;
+  EXPECT_EQ(correct.rows(), res.rows());
+  EXPECT_EQ(correct.cols(), res.cols());
+  EXPECT_MATRIX_NEAR(correct,res,1e-9);
+}

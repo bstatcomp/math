4de1056f4fb33f00cbe979d13af19c5385737822
tadej <tadej.c@gmail.com>
2019-09-06 10:29:54 +0200

    simplified a test

--- test/unit/math/opencl/kernel_generator/operation_test.cpp
+++ test/unit/math/opencl/kernel_generator/operation_test.cpp
@@ -1,8 +1,6 @@
 #ifdef STAN_OPENCL
 
 #include <stan/math/opencl/kernel_generator/binary_operation.hpp>
-#include <stan/math/opencl/kernel_generator/unary_function.hpp>
-#include <stan/math/opencl/kernel_generator/scalar.hpp>
 #include <stan/math/opencl/matrix_cl.hpp>
 #include <stan/math/opencl/copy.hpp>
 #include <Eigen/Dense>
@@ -26,12 +24,12 @@ TEST(MathMatrixCL, reuse_expression){
   matrix_cl<double> m1_cl(m1);
   matrix_cl<double> m2_cl(m2);
   auto tmp = m1_cl + 0.1234 + m2_cl;
-  matrix_cl<double> res_cl = 3*tmp + sin(tmp);
+  matrix_cl<double> res_cl = stan::math::elewise_multiplication(tmp - 6, tmp);
 
   MatrixXd res = stan::math::from_matrix_cl(res_cl);
 
   auto tmp_eig = m1.array() + 0.1234 + m2.array();
-  MatrixXd res_eig = 3*tmp_eig + sin(tmp_eig);
+  MatrixXd res_eig = (tmp_eig - 6)*tmp_eig;
 
   EXPECT_MATRIX_NEAR(res_eig,res,1e-9);
 }

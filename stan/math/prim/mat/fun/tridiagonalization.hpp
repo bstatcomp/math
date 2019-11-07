db69c3b1b7c2b44f705ad6afce8f1947b29ae188
tadej <tadej.c@gmail.com>
2019-11-07 11:01:37 +0100

    generalized tridiagonalization to work with complex numbers

--- stan/math/prim/mat/fun/tridiagonalization.hpp
+++ stan/math/prim/mat/fun/tridiagonalization.hpp
@@ -4,6 +4,7 @@
 #include <stan/math/prim/scal/fun/constants.hpp>
 #include <Eigen/Dense>
 #include <algorithm>
+#include <ccomplex>
 #include <cmath>
 
 namespace stan {
@@ -11,70 +12,82 @@ namespace math {
 namespace internal {
 
 /**
- * Tridiagonalize a symmetric matrix using block Housholder algorithm. A = Q * T
- * * Q^T, where T is tridiagonal and Q is orthonormal.
+ * Tridiagonalize a selfadjoint matrix using block Housholder algorithm. A = Q *
+ * T
+ * * Q^T, where T is tridiagonal and Q is unitary.
  * @param A Input matrix
  * @param[out] packed Packed form of the tridiagonal matrix. Elements of the
- * resulting symmetric tridiagonal matrix T are in the diagonal and first
- * superdiagonal. Columns bellow diagonal contain householder vectors that can
- * be used to construct orthogonal matrix Q.
+ * resulting selfadjoint tridiagonal matrix T are in the diagonal and first
+ * superdiagonal, which contains subdiagonal entries of T. Columns bellow
+ * diagonal contain householder vectors that can be used to construct unitary
+ * matrix Q.
  * @param r Block size. Affects only performance of the algorithm. Optimal value
  * depends on the size of A and cache of the processor. For larger matrices or
  * larger cache sizes a larger value is optimal.
  */
-void block_householder_tridiag(const Eigen::MatrixXd& A,
-                               Eigen::MatrixXd& packed, const int r = 60) {
+template <typename Scalar>
+void block_householder_tridiag(
+    const Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic>& A,
+    Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic>& packed,
+    const int r = 60) {
+  using Real = typename Eigen::NumTraits<Scalar>::Real;
   packed = A;
   for (size_t k = 0; k < packed.rows() - 2; k += r) {
     const int actual_r = std::min({r, static_cast<int>(packed.rows() - k - 2)});
-    Eigen::MatrixXd V(packed.rows() - k - 1, actual_r);
+    Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic> V(
+        packed.rows() - k - 1, actual_r);
 
     for (size_t j = 0; j < actual_r; j++) {
       auto householder = packed.col(k + j).tail(packed.rows() - k - j - 1);
       if (j != 0) {
         auto householder_whole = packed.col(k + j).tail(packed.rows() - k - j);
-        householder_whole -= packed.block(j + k, k, householder_whole.size(), j)
-                                 * V.block(j - 1, 0, 1, j).transpose()
+        householder_whole -= packed.block(k + j, k, householder_whole.size(), j)
+                                 * V.block(j - 1, 0, 1, j).adjoint()
                              + V.block(j - 1, 0, householder_whole.size(), j)
-                                   * packed.block(j + k, k, 1, j).transpose();
+                                   * packed.block(k + j, k, 1, j).adjoint();
       }
-      double q = householder.squaredNorm();
-      const double alpha = -std::copysign(sqrt(q), packed(k + j, k + j));
-      q -= householder[0] * householder[0];
+      Real q = householder.squaredNorm();
+      Scalar alpha = -sqrt(q);
+      if (householder[0] != 0.) {
+        alpha *= householder[0] / Eigen::numext::abs(householder[0]);
+      }
+
+      q -= std::norm(householder[0]);
       householder[0] -= alpha;
-      q += householder[0] * householder[0];
+      q += std::norm(householder[0]);
       q = sqrt(q);
-      if (q != 0) {
+      if (q != 0.) {
         householder *= SQRT_2 / q;
       }
 
       auto& u = householder;
-      Eigen::VectorXd v(householder.size() + 1);
+      Eigen::Matrix<Scalar, Eigen::Dynamic, 1> v(householder.size() + 1);
       v.tail(householder.size())
           = packed.bottomRightCorner(packed.rows() - k - j - 1,
                                      packed.cols() - k - j - 1)
-                    .selfadjointView<Eigen::Lower>()
+                    .template selfadjointView<Eigen::Lower>()
                 * u
             - packed.block(k + j + 1, k, u.size(), j)
-                  * (V.bottomLeftCorner(u.size(), j).transpose() * u)
+                  * (V.bottomLeftCorner(u.size(), j).adjoint() * u)
             - V.bottomLeftCorner(u.size(), j)
-                  * (packed.block(k + j + 1, k, u.size(), j).transpose() * u);
+                  * (packed.block(k + j + 1, k, u.size(), j).adjoint() * u);
       v[0] = q / SQRT_2;
-      const double cnst = v.tail(householder.size()).transpose() * u;
+      const Real cnst = (v.tail(householder.size()).adjoint() * u).real()[0];
       v.tail(householder.size()) -= 0.5 * cnst * u;
 
+      // calculate subdiagonal of T into superdiagonal of packed
       packed(k + j, k + j + 1)
           = packed(k + j + 1, k + j) * q / SQRT_2 + alpha - v[0] * u[0];
       V.col(j).tail(V.rows() - j) = v.tail(V.rows() - j);
     }
-    Eigen::MatrixXd partial_update
+    Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic> partial_update
         = packed.block(k + actual_r, k, packed.rows() - k - actual_r, actual_r)
-          * V.bottomRows(V.rows() - actual_r + 1).transpose();
+          * V.bottomRows(V.rows() - actual_r + 1).adjoint();
     packed
         .block(k + actual_r, k + actual_r, packed.rows() - k - actual_r,
                packed.cols() - k - actual_r)
-        .triangularView<Eigen::Lower>()
-        -= partial_update + partial_update.transpose();
+        .template triangularView<Eigen::Lower>()
+        -= partial_update + partial_update.adjoint();
   }
   packed(packed.rows() - 2, packed.cols() - 1)
       = packed(packed.rows() - 1, packed.cols() - 2);
@@ -84,33 +97,37 @@ void block_householder_tridiag(const Eigen::MatrixXd& A,
  * Calculates Q*A in place. To construct Q pass identity matrix as input A.
  * @param packed Packed result of tridiagonalization that contains householder
  * vectors that define Q in columns bellow the diagonal. Usually result of a
- * call to `block_householder_tridiag3`.
+ * call to `block_householder_tridiag`.
  * @param[in,out] A On input a matrix to multiply with Q. On output the product
  * Q*A.
  * @param r Block size. Affects only performance of the algorithm. Optimal value
  * depends on the size of A and cache of the processor. For larger matrices or
  * larger cache sizes a larger value is optimal.
  */
-void block_apply_packed_Q(const Eigen::MatrixXd& packed, Eigen::MatrixXd& A,
-                          const int r = 100) {
-  // if input A==Identity, constructs Q
-  Eigen::MatrixXd scratch_space(A.rows(), r);
+template <typename Scalar>
+void block_apply_packed_Q(
+    const Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic>& packed,
+    Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic>& A,
+    const int r = 100) {
+  Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic> scratch_space(A.rows(),
+                                                                      r);
   for (int k = (packed.rows() - 3) / r * r; k >= 0; k -= r) {
     const int actual_r = std::min({r, static_cast<int>(packed.rows() - k - 2)});
-    Eigen::MatrixXd W(packed.rows() - k - 1, actual_r);
+    Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic> W(
+        packed.rows() - k - 1, actual_r);
     W.col(0) = packed.col(k).tail(W.rows());
     for (size_t j = 1; j < actual_r; j++) {
       scratch_space.col(0).head(j).noalias()
-          = packed.block(k + j + 1, k, packed.rows() - k - j - 1, j).transpose()
-            * packed.col(j + k).tail(packed.rows() - k - j - 1);
+          = packed.block(k + j + 1, k, packed.rows() - k - j - 1, j).adjoint()
+            * packed.col(k + j).tail(packed.rows() - k - j - 1);
       W.col(j).noalias() = -W.leftCols(j) * scratch_space.col(0).head(j);
       W.col(j).tail(W.rows() - j)
-          += packed.col(j + k).tail(packed.rows() - k - j - 1);
+          += packed.col(k + j).tail(packed.rows() - k - j - 1);
     }
     scratch_space.transpose().bottomRows(actual_r).noalias()
         = packed.block(k + 1, k, packed.rows() - k - 1, actual_r)
-              .transpose()
-              .triangularView<Eigen::Upper>()
+              .adjoint()
+              .template triangularView<Eigen::Upper>()
           * A.bottomRows(A.rows() - k - 1);
     A.bottomRows(A.cols() - k - 1).noalias()
         -= W * scratch_space.transpose().bottomRows(actual_r);
--- test/unit/math/prim/mat/fun/tridiagonalization_test.cpp
+++ test/unit/math/prim/mat/fun/tridiagonalization_test.cpp
@@ -50,3 +50,43 @@ TEST(MathMatrix, tridiagonalization_large) {
       (q * q.transpose()).isApprox(Eigen::MatrixXd::Identity(size, size)));
   EXPECT_TRUE((q * t * q.transpose()).isApprox(input));
 }
+
+TEST(MathMatrix, tridiagonalization_small_complex) {
+  int size = 7;
+  srand(0);  // ensure test repeatability
+  Eigen::MatrixXcd input = Eigen::MatrixXcd::Random(size, size);
+  input += input.adjoint().eval();
+  Eigen::MatrixXcd packed;
+
+  stan::math::internal::block_householder_tridiag(input, packed);
+  Eigen::MatrixXcd q = Eigen::MatrixXcd::Identity(size, size);
+  stan::math::internal::block_apply_packed_Q(packed, q);
+
+  Eigen::MatrixXcd t = Eigen::MatrixXcd::Constant(size, size, 0);
+  t.diagonal() = packed.diagonal();
+  t.diagonal(1) = packed.diagonal(1).conjugate();
+  t.diagonal(-1) = packed.diagonal(1);
+  EXPECT_TRUE(
+      (q * q.adjoint()).isApprox(Eigen::MatrixXcd::Identity(size, size)));
+  EXPECT_TRUE((q * t * q.adjoint()).isApprox(input));
+}
+
+TEST(MathMatrix, tridiagonalization_big_complex) {
+  int size = 345;
+  srand(0);  // ensure test repeatability
+  Eigen::MatrixXcd input = Eigen::MatrixXcd::Random(size, size);
+  input += input.adjoint().eval();
+  Eigen::MatrixXcd packed;
+
+  stan::math::internal::block_householder_tridiag(input, packed);
+  Eigen::MatrixXcd q = Eigen::MatrixXcd::Identity(size, size);
+  stan::math::internal::block_apply_packed_Q(packed, q);
+
+  Eigen::MatrixXcd t = Eigen::MatrixXcd::Constant(size, size, 0);
+  t.diagonal() = packed.diagonal();
+  t.diagonal(1) = packed.diagonal(1).conjugate();
+  t.diagonal(-1) = packed.diagonal(1);
+  EXPECT_TRUE(
+      (q * q.adjoint()).isApprox(Eigen::MatrixXcd::Identity(size, size)));
+  EXPECT_TRUE((q * t * q.adjoint()).isApprox(input));
+}

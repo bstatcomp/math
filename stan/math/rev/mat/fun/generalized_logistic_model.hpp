0027665486c17a04d1d683ab368cbcd5c0fdbd8c
rok-cesnovar <rok.cesnovar@fri.uni-lj.si>
2020-04-02 10:03:27 +0200

    3

--- stan/math/rev/mat/fun/generalized_logistic_model.hpp
+++ stan/math/rev/mat/fun/generalized_logistic_model.hpp
@@ -133,7 +133,7 @@ inline var generalized_logistic_model(
     matrix_cl<double> IDs_cl(opencl_context.IDs(IDs_copy), 1, N);
     matrix_cl<double> is_pbo_cl(opencl_context.is_pbo(is_pbo_copy), 1, N);
     
-    Eigen::Matrix a = from_matrix_cl(IDp_cl);
+    Eigen::Matrix<int> a = from_matrix_cl(IDp_cl);
     std::cout << a[0] << ", " << a[1] << ", " << a[2] << std::endl;
 
     matrix_d t1 = eta_ps.val();  matrix_d t2 = eta_ss.val();

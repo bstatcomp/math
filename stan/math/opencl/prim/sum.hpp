50396d83b0acf5b5ada6feee43cd2ee0ce124467
tadej <tadej.c@gmail.com>
2021-02-04 13:23:03 +0100

    reimplemented sum

--- stan/math/opencl/prim/sum.hpp
+++ stan/math/opencl/prim/sum.hpp
@@ -19,16 +19,7 @@ namespace math {
 template <typename T,
           require_all_kernel_expressions_and_none_scalar_t<T>* = nullptr>
 value_type_t<T> sum(const T& m) {
-  matrix_cl<value_type_t<T>> res;
-  if (m.rows() == 1) {
-    // using rowwise_sum would run just 1 thread
-    res = colwise_sum(transpose(m));
-  } else if (m.cols() == 1) {
-    res = colwise_sum(m);
-  } else {
-    res = colwise_sum(rowwise_sum(m));
-  }
-  return sum(from_matrix_cl(res));
+  return sum(from_matrix_cl(colwise_sum(reshape(m,m.size(),1))));
 }
 
 }  // namespace math
